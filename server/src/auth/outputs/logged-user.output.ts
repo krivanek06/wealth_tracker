442e1ad37c0274dfd80c6e7ca6a385cfc6dd5ca4
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-13 06:44:16 +0300

    feat(authentication): basic authentication works

--- server/src/auth/outputs/logged-user.output.ts
+++ server/src/auth/outputs/logged-user.output.ts
@@ -1,7 +1,11 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { User } from './../../modules/user';
 
 @ObjectType()
 export class LoggedUserOutput {
-	@Field(() => String, { description: "Generated user's accessToken" })
+	@Field(() => String, { description: "Generated user's accessToken, encoded RequestUser" })
 	accessToken: string;
+
+	@Field(() => User, { description: 'Return the authenticated user object' })
+	user: User;
 }

483e093fa4b40d40b2f5a265f4e8bd44e925b939
krivanek06 <krivanek1234@gmail.com>
2022-10-14 09:30:59 +0300

    feat(personal-account): create personal account

--- server/src/modules/personal-account/personal-account/personal-account.module.ts
+++ server/src/modules/personal-account/personal-account/personal-account.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { PersonalAccountMonthlyResolver } from './personal-account-monthly.resolver';
 import { PersonalAccountMonthlyService } from './personal-account-monthly.service';
 import { PersonalAccountResolver } from './personal-account.resolver';
 import { PersonalAccountService } from './personal-account.service';
 
 @Module({
-	providers: [PersonalAccountService, PersonalAccountResolver, PersonalAccountMonthlyService, PrismaService],
-	exports: [PersonalAccountResolver],
+	providers: [
+		PersonalAccountService,
+		PersonalAccountResolver,
+		PersonalAccountMonthlyResolver,
+		PersonalAccountMonthlyService,
+		PrismaService,
+	],
+	exports: [PersonalAccountResolver, PersonalAccountMonthlyResolver],
 })
 export class PersonalAccountModule {}
--- server/src/modules/personal-account/personal-account/personal-account.service.ts
+++ server/src/modules/personal-account/personal-account/personal-account.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { PersonalAccountCreateInput } from './dto';
 import { PersonalAccount } from './entity';
 
 @Injectable()
 export class PersonalAccountService {
 	constructor(private prisma: PrismaService) {}
+
+	createPersonalAccount({ name }: PersonalAccountCreateInput, userId: string): Promise<PersonalAccount> {
+		return this.prisma.personalAccount.create({
+			data: {
+				name,
+				userId,
+			},
+		});
+	}
 
 	getPersonalAccounts(userId: string): Promise<PersonalAccount[]> {
 		return this.prisma.personalAccount.findMany({

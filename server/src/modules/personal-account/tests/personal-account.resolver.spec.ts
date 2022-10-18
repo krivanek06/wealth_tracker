1f00d982eb5191a60a539fa122478a4a869b1da0
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-18 18:36:53 +0300

    test(server): all tests are running

--- server/src/modules/personal-account/tests/personal-account.resolver.spec.ts
+++ server/src/modules/personal-account/tests/personal-account.resolver.spec.ts
@@ -1,12 +1,23 @@
+import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonalAccountResolver } from '../resolvers/personal-account.resolver';
+import { PersonalAccountMonthlyService, PersonalAccountService, PersonalAccountWeeklyService } from '../services';
 
 describe('PersonalAccountResolver', () => {
 	let resolver: PersonalAccountResolver;
 
+	const personalAccountServiceMock = createMock<PersonalAccountService>();
+	const personalAccountMonthlyServiceMock = createMock<PersonalAccountMonthlyService>();
+	const personalAccountWeeklyServiceMock = createMock<PersonalAccountWeeklyService>();
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [PersonalAccountResolver],
+			providers: [
+				PersonalAccountResolver,
+				{ provide: PersonalAccountService, useValue: personalAccountServiceMock },
+				{ provide: PersonalAccountMonthlyService, useValue: personalAccountMonthlyServiceMock },
+				{ provide: PersonalAccountWeeklyService, useValue: personalAccountWeeklyServiceMock },
+			],
 		}).compile();
 
 		resolver = module.get<PersonalAccountResolver>(PersonalAccountResolver);

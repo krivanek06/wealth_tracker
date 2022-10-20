64ac9cb25fd34658a8defa5bbbd0586d52639e76
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-20 19:54:39 +0300

    test: added test for investment-account-history.service.spec.ts|

--- server/src/modules/investment-account/tests/investment-account-history.service.spec.ts
+++ server/src/modules/investment-account/tests/investment-account-history.service.spec.ts
@@ -1,12 +1,33 @@
 import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { when } from 'jest-when';
 import { PrismaService } from '../../../prisma';
+import { InvestmentAccount, InvestmentAccountHistory } from '../entities';
 import { InvestmentAccountHistoryService } from '../services/investment-account-history.service';
 
 describe('InvestmentAccountHistoryService', () => {
 	let service: InvestmentAccountHistoryService;
 
-	const prismaServiceMock: PrismaService = createMock<PrismaService>({});
+	const investmentAccountHistoryIdMock = '1234';
+	const investmentAccountIdMock = 'poiuewewew';
+
+	const investmentAccountMock: InvestmentAccount = {
+		id: investmentAccountIdMock,
+	} as InvestmentAccount;
+
+	const investmentAccountHistoryMock: InvestmentAccountHistory = {
+		id: investmentAccountHistoryIdMock,
+		investmentAccountId: investmentAccountIdMock,
+		portfolioSnapshots: [],
+		portfolioSnapshotTotal: 0,
+	};
+
+	const prismaServiceMock: PrismaService = createMock<PrismaService>({
+		investmentAccountHistory: {
+			findFirst: jest.fn(),
+			create: jest.fn().mockResolvedValue(investmentAccountHistoryMock),
+		},
+	});
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -14,9 +35,49 @@ describe('InvestmentAccountHistoryService', () => {
 		}).compile();
 
 		service = module.get<InvestmentAccountHistoryService>(InvestmentAccountHistoryService);
+
+		when(prismaServiceMock.investmentAccountHistory.findFirst)
+			.calledWith({
+				where: {
+					investmentAccountId: investmentAccountIdMock,
+				},
+			})
+			.mockResolvedValue(investmentAccountHistoryMock);
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
 	});
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	describe('Test: getInvestmentAccountHistoryInvestmentAccount()', () => {
+		it('should return investmentAccountHistory by investmentAccountId', async () => {
+			const result = await service.getInvestmentAccountHistoryInvestmentAccount(investmentAccountMock);
+
+			expect(result).toStrictEqual(investmentAccountHistoryMock);
+			expect(prismaServiceMock.investmentAccountHistory.findFirst).toHaveBeenCalledWith({
+				where: {
+					investmentAccountId: investmentAccountIdMock,
+				},
+			});
+		});
+	});
+
+	describe('Test: createInvestmentAccountHistory()', () => {
+		it('should create an investmentAccountHistory', async () => {
+			const result = await service.createInvestmentAccountHistory(investmentAccountMock);
+
+			expect(result).toStrictEqual(investmentAccountHistoryMock);
+			expect(prismaServiceMock.investmentAccountHistory.create).toHaveBeenCalledWith({
+				data: {
+					investmentAccountId: investmentAccountMock.id,
+					portfolioSnapshotTotal: 0,
+					portfolioSnapshots: [],
+				},
+			});
+		});
 	});
 });

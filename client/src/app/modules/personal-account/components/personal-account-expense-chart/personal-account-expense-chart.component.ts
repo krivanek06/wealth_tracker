45bfb21b677b7d02fde22366e87401552314f181
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-28 10:06:12 +0300

    feat(personal-account): added expense chart template

--- client/src/app/modules/personal-account/components/personal-account-expense-chart/personal-account-expense-chart.component.ts
+++ client/src/app/modules/personal-account/components/personal-account-expense-chart/personal-account-expense-chart.component.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { chartColors1, SwimlaneChartData } from './../../../../shared/models';
 
 @Component({
-  selector: 'app-personal-account-expense-chart',
-  templateUrl: './personal-account-expense-chart.component.html',
-  styleUrls: ['./personal-account-expense-chart.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-personal-account-expense-chart',
+	templateUrl: './personal-account-expense-chart.component.html',
+	styleUrls: ['./personal-account-expense-chart.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonalAccountExpenseChartComponent implements OnInit {
+	@Input() data!: SwimlaneChartData[];
+	colorScheme: any = {
+		domain: chartColors1,
+	};
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- client/src/app/modules/personal-account/pages/personal-account/personal-account.component.ts
+++ client/src/app/modules/personal-account/pages/personal-account/personal-account.component.ts
@@ -16,7 +16,7 @@ export class PersonalAccountComponent implements OnInit {
 			return { name: d.tagName, value: d.value } as SwimlaneChartDataSeries;
 		});
 
-		// get weekly aggregation
+		// get weekly income aggregation
 		const weeklyIncomeSeries: SwimlaneChartDataSeries[] = data.weeklyAggregaton.map((d) => {
 			// add together weekly income and expense
 			const weeklyIncome = d.data.reduce(
@@ -26,10 +26,43 @@ export class PersonalAccountComponent implements OnInit {
 			return { name: d.id, value: weeklyIncome };
 		});
 		this.weeklyChartData = { name: data.name, series: weeklyIncomeSeries };
+
+		// get weekly expense aggregation
+		/*
+          [{
+            "name": "Shopping",
+            "series": [
+              {
+                "value": 2983,
+                "name": "2022-9-42"
+              },
+            ]
+          }]
+    */
+		this.weeklyExpenseChartData = data.weeklyAggregaton.reduce((acc, curr) => {
+			curr.data
+				.filter((d) => d.tagType === TagDataType.Expense)
+				.forEach((d) => {
+					// index of tagName in accumulation
+					const savedTagIndex = acc.findIndex((accData) => accData.name === d.tagName);
+
+					if (savedTagIndex === -1) {
+						// tag not yet save = new data
+						const newData = { name: d.tagName, series: [{ name: curr.id, value: d.value }] } as SwimlaneChartData;
+						acc.push(newData);
+					} else {
+						// add another series into saved tag array
+						acc[savedTagIndex].series.push({ name: curr.id, value: d.value });
+					}
+				});
+
+			return acc;
+		}, [] as SwimlaneChartData[]);
 	}
 
 	yearlyChartData!: SwimlaneChartDataSeries[];
 	weeklyChartData!: SwimlaneChartData;
+	weeklyExpenseChartData!: SwimlaneChartData[];
 
 	constructor(private personalAccountApiService: PersonalAccountApiService) {}
 

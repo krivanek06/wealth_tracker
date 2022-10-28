0ebdb3f1c2dbb2f8833b703df1187e91aae5abee
krivanek06 <krivanek1234@gmail.com>
2022-10-28 10:06:12 +0300

    feat(personal-account): added expense chart template

--- client/src/app/modules/personal-account/components/personal-account-expense-chart/personal-account-expense-chart.module.ts
+++ client/src/app/modules/personal-account/components/personal-account-expense-chart/personal-account-expense-chart.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PersonalAccountExpenseChartComponent } from './personal-account-expense-chart.component';
 
 @NgModule({
 	declarations: [PersonalAccountExpenseChartComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, NgxChartsModule],
 	exports: [PersonalAccountExpenseChartComponent],
 })
 export class PersonalAccountExpenseChartModule {}

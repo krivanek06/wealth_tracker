37052cf1202db959ff1146fcecfaf0f29913ff67
krivanek06 <krivanek1234@gmail.com>
2022-10-28 08:57:31 +0300

    feat(personal-account): template for personal-account-growth-chart

--- client/src/app/modules/personal-account/components/personal-account-growth-chart/personal-account-growth-chart.component.ts
+++ client/src/app/modules/personal-account/components/personal-account-growth-chart/personal-account-growth-chart.component.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { chartColors1, SwimlaneChartData } from './../../../../shared/models';
 
 @Component({
-  selector: 'app-personal-account-growth-chart',
-  templateUrl: './personal-account-growth-chart.component.html',
-  styleUrls: ['./personal-account-growth-chart.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-personal-account-growth-chart',
+	templateUrl: './personal-account-growth-chart.component.html',
+	styleUrls: ['./personal-account-growth-chart.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonalAccountGrowthChartComponent implements OnInit {
+	@Input() data!: SwimlaneChartData[];
+	colorScheme: any = {
+		domain: chartColors1,
+	};
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- client/src/app/modules/personal-account/components/personal-account-growth-chart/personal-account-growth-chart.module.ts
+++ client/src/app/modules/personal-account/components/personal-account-growth-chart/personal-account-growth-chart.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PersonalAccountGrowthChartComponent } from './personal-account-growth-chart.component';
 
 @NgModule({
 	declarations: [PersonalAccountGrowthChartComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, NgxChartsModule],
 	exports: [PersonalAccountGrowthChartComponent],
 })
 export class PersonalAccountGrowthChartModule {}

2d613d0ea16e9897dc30973fbf34984ebc527980
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-29 09:43:15 +0300

    feat(personal-account): client display tempalte table

--- client/src/app/modules/personal-account/components/personal-account-daily-entries-table/personal-account-daily-entries-table.module.ts
+++ client/src/app/modules/personal-account/components/personal-account-daily-entries-table/personal-account-daily-entries-table.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { PersonalAccountDailyEntriesTableComponent } from './personal-account-daily-entries-table.component';
 
 @NgModule({
 	declarations: [PersonalAccountDailyEntriesTableComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, MatTableModule, MatRippleModule, MatSortModule],
 	exports: [PersonalAccountDailyEntriesTableComponent],
 })
 export class PersonalAccountDailyEntriesTableModule {}

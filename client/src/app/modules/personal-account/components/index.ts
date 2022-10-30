bb17581a902e0e98b712bef17ec93d7db08195ec
krivanek06 <krivanek1234@gmail.com>
2022-10-30 10:38:07 +0200

    feat(chart): removed personal-account-expense

--- client/src/app/modules/personal-account/components/index.ts
+++ client/src/app/modules/personal-account/components/index.ts
@@ -2,5 +2,4 @@ export * from './personal-account-aggregation-radio-buttons/personal-account-agg
 export * from './personal-account-daily-entries-filter/personal-account-daily-entries-filter.module';
 export * from './personal-account-daily-entries-table/personal-account-daily-entries-table.module';
 export * from './personal-account-expense-allocation-chart/personal-account-expense-allocation-chart.module';
-export * from './personal-account-expense-chart/personal-account-expense-chart.module';
 export * from './personal-account-overview-chart/personal-account-overview-chart.module';

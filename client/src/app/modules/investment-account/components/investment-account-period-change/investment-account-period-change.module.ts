c6123e0a1932fef2f26e0f6c738f86f06049e76c
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-12-01 10:28:54 +0200

    feat: fixed UI

---
+++ client/src/app/modules/investment-account/components/investment-account-period-change/investment-account-period-change.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InvestmentAccountPeriodChangeComponent } from './investment-account-period-change.component';
+
+
+
+@NgModule({
+  declarations: [
+    InvestmentAccountPeriodChangeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InvestmentAccountPeriodChangeModule { }

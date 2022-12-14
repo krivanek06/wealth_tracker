a720fd94bc21585fe965066c8a2e700ea4ce44cb
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-12-14 13:21:16 +0200

    feat: authentication in-progress

--- client/src/app/modules/user-settings/modals/login-modal/login-modal.component.ts
+++ client/src/app/modules/user-settings/modals/login-modal/login-modal.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { filter, switchMap, tap } from 'rxjs';
+import { AuthenticationFacadeService } from '../../../../core/auth';
+import { LoginUserInput, RegisterUserInput } from '../../../../core/graphql';
+import { DialogServiceUtil } from './../../../../shared/dialogs/dialog-service.util';
 
 @Component({
 	selector: 'app-login-modal',
@@ -8,7 +13,39 @@ import { MatDialogRef } from '@angular/material/dialog';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginModalComponent implements OnInit {
-	constructor(private dialogRef: MatDialogRef<LoginModalComponent>) {}
+	loginUserInputControl = new FormControl<LoginUserInput | null>(null);
+	registerUserInputControl = new FormControl<RegisterUserInput | null>(null);
 
-	ngOnInit(): void {}
+	constructor(
+		private authenticationFacadeService: AuthenticationFacadeService,
+		private dialogRef: MatDialogRef<LoginModalComponent>
+	) {}
+
+	ngOnInit(): void {
+		this.loginUserInputControl.valueChanges
+			.pipe(
+				filter((res): res is LoginUserInput => !!res),
+				switchMap((res) =>
+					this.authenticationFacadeService
+						.loginUserBasic(res)
+						.pipe(tap(() => DialogServiceUtil.showNotificationBar(`You have been successfully logged in`, 'success')))
+				)
+			)
+			.subscribe();
+
+		this.registerUserInputControl.valueChanges
+			.pipe(
+				filter((res): res is RegisterUserInput => !!res),
+				switchMap((res) =>
+					this.authenticationFacadeService
+						.registerBasic(res)
+						.pipe(
+							tap((res) =>
+								DialogServiceUtil.showNotificationBar(`Account ${res.email} has been successfully created`, 'success')
+							)
+						)
+				)
+			)
+			.subscribe();
+	}
 }

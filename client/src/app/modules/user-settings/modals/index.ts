b919649f87c0905995db80a198d0cf8243290066
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-12-27 10:08:47 +0100

    feat(client): show user profile dialog

--- client/src/app/modules/user-settings/modals/index.ts
+++ client/src/app/modules/user-settings/modals/index.ts
@@ -1,2 +1,4 @@
 export * from './login-modal/login-modal.component';
 export * from './login-modal/login-modal.module';
+export * from './user-profile-modal/user-profile-modal.component';
+export * from './user-profile-modal/user-profile-modal.module';

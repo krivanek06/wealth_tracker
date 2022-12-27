390c3713f0dc8dc5f582781921f6c64b2f3e964b
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-12-27 09:54:53 +0100

    feat(server): implemented changed password

--- server/src/providers/sendgrid/sendgrid.service.ts
+++ server/src/providers/sendgrid/sendgrid.service.ts
@@ -15,18 +15,4 @@ export class SendGridService {
 		console.log(`Email successfully dispatched to ${mail.to}`);
 		return transport;
 	}
-
-	createResetPasswordEmail = (email: string, password: string): SendGrid.MailDataRequired => {
-		const mail = {
-			to: email,
-			subject: 'Reset password',
-			from: this.senderEmail,
-			html: `
-                <h1>You password has been reset</h1>
-                <div>Your current password is: ${password}</div>
-            `,
-		};
-
-		return mail;
-	};
 }

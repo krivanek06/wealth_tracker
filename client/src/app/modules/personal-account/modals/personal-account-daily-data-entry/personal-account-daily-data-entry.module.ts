0682223ba4604243c314292a7638cf86bd249a5f
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-02-26 17:40:06 +0200

    chore: fixed tooltip

--- client/src/app/modules/personal-account/modals/personal-account-daily-data-entry/personal-account-daily-data-entry.module.ts
+++ client/src/app/modules/personal-account/modals/personal-account-daily-data-entry/personal-account-daily-data-entry.module.ts
@@ -6,12 +6,13 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import {
 	DatePickerComponent,
 	FormMatInputWrapperModule,
 	NumberKeyboardComponent,
 } from './../../../../shared/components';
-import { DefaultImgDirective, TooltipDirective } from './../../../../shared/directives';
+import { DefaultImgDirective } from './../../../../shared/directives';
 import { DailyDataEntryDisplayElementsComponent } from './daily-data-entry-display-elements/daily-data-entry-display-elements.component';
 import { PersonalAccountDailyDataEntryComponent } from './personal-account-daily-data-entry.component';
 
@@ -29,7 +30,7 @@ import { PersonalAccountDailyDataEntryComponent } from './personal-account-daily
 		NumberKeyboardComponent,
 		FormMatInputWrapperModule,
 		DatePickerComponent,
-		TooltipDirective,
+		MatTooltipModule,
 	],
 })
 export class PersonalAccountDailyDataEntryModule {}

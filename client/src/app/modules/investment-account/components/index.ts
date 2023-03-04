2f15b6e3f063d7c73d502bf252401deb8fd79ffb
krivanek06 <krivanek1234@gmail.com>
2023-03-04 13:35:36 +0200

    chore: investment-account skeleton loading

--- client/src/app/modules/investment-account/components/index.ts
+++ client/src/app/modules/investment-account/components/index.ts
@@ -1,4 +1,4 @@
 export * from './investment-account-active-holdings-table/investment-account-active-holdings-table.module';
 export * from './investment-account-period-change/investment-account-period-change.module';
 export * from './investment-account-portfolio-growth-chart/investment-account-portfolio-growth-chart.module';
-export * from './investment-account-state/investment-account-state.module';
+export * from './investment-account-state/investment-account-state.component';

0d55b1de2daf94fb8060186ccadee48047c21a27
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-03-12 19:02:14 +0200

    chore: creating welcome page

--- client/src/app/core/guards/verify-authentication.guard.ts
+++ client/src/app/core/guards/verify-authentication.guard.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationFacadeService } from '../auth';
 import { LoggedUserOutputFragment } from '../graphql';
-import { STORAGE_ACCESS_TOKEN } from '../models';
+import { STORAGE_ACCESS_TOKEN, TOP_LEVEL_NAV } from '../models';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class ResolveTokenGuard implements Resolve<void> {
+export class VerifyAuthentication implements CanActivate {
 	constructor(private authenticationFacadeService: AuthenticationFacadeService, private router: Router) {}
 
-	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<void> | Promise<void> | void {
+	canActivate(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot
+	): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 		if (this.getTokenFromLocalStorage()) {
-			return;
+			this.router.navigate([TOP_LEVEL_NAV.welcome]);
+			return false;
 		}
 
 		const accessToken = route.queryParamMap.get('accessToken');
 		console.log('client got token', accessToken);
 
 		if (!accessToken) {
-			return;
+			this.router.navigate([TOP_LEVEL_NAV.welcome]);
+			return false;
 		}
 
-		//save token
+		// save token
 		this.authenticationFacadeService.setAccessToken({
 			__typename: 'LoggedUserOutput',
 			accessToken,
@@ -36,12 +41,14 @@ export class ResolveTokenGuard implements Resolve<void> {
 			},
 			queryParamsHandling: 'merge',
 		});
+
+		return true;
 	}
 
 	private getTokenFromLocalStorage(): boolean {
 		const token = localStorage.getItem(STORAGE_ACCESS_TOKEN);
 		if (token) {
-			console.log('token inside localstorage');
+			console.log('token inside local storage');
 			try {
 				const parsed = JSON.parse(token) as LoggedUserOutputFragment;
 				this.authenticationFacadeService.setAccessToken(parsed);

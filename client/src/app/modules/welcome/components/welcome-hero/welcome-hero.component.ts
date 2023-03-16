c8a23cc9d27beabe591f18229d879110c5775170
krivanek06 <krivanek1234@gmail.com>
2023-03-16 09:22:26 +0200

    chore: adding login on welcome page

--- client/src/app/modules/welcome/components/welcome-hero/welcome-hero.component.ts
+++ client/src/app/modules/welcome/components/welcome-hero/welcome-hero.component.ts
@@ -1,14 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { TOP_LEVEL_NAV } from '../../../../core/models';
+import { LoginModalComponent } from '../../../user-settings/modals';
 
 @Component({
-  selector: 'app-welcome-hero',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './welcome-hero.component.html',
-  styleUrls: ['./welcome-hero.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-welcome-hero',
+	standalone: true,
+	imports: [CommonModule, MatButtonModule],
+	templateUrl: './welcome-hero.component.html',
+	styleUrls: ['./welcome-hero.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WelcomeHeroComponent {
+	constructor(private dialog: MatDialog, private router: Router) {}
 
+	onLogin() {
+		this.dialog
+			.open(LoginModalComponent, {
+				panelClass: ['g-mat-dialog-small'],
+			})
+			.afterClosed()
+			.subscribe((res: boolean) => {
+				if (res) {
+					this.router.navigate([TOP_LEVEL_NAV.dashboard]);
+				}
+			});
+	}
 }

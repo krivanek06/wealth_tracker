aef8e3067e0d1b618995b2edc5b6d3a0ec4a1002
krivanek06 <krivanek1234@gmail.com>
2023-04-04 13:26:48 +0300

    feat: adding Poppins fonts

--- client/src/app/shared/directives/index.ts
+++ client/src/app/shared/directives/index.ts
@@ -2,4 +2,3 @@ export * from './default-img.directive';
 export * from './perceptage-increase.directive';
 export * from './range.directive';
 export * from './StylePaginatorDirective.directive';
-export * from './tooltip.directive';

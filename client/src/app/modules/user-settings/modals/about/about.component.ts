2d3784998b968d9e333d15d1d7855ece29f7f043
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-04-05 02:41:43 +0300

    chore: added about-project into app

---
+++ client/src/app/modules/user-settings/modals/about/about.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { URL_LINKED_IN, URL_PERSONAL_WEBSITE } from '../../../../core/models';
+
+@Component({
+	selector: 'app-about',
+	standalone: true,
+	imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule, MatDividerModule, MatTooltipModule],
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutComponent {
+	URL_LINKED_IN = URL_LINKED_IN;
+	URL_PERSONAL_WEBSITE = URL_PERSONAL_WEBSITE;
+	constructor(private dialogRef: MatDialogRef<AboutComponent>) {}
+}

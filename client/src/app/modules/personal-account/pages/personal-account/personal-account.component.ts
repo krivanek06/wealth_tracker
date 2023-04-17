e2ee6bf734ccdc400f2c984c45aa5b831652d0b9
krivanek06 <krivanek1234@gmail.com>
2023-04-17 22:36:20 +0300

    chore: rename component selector

--- client/src/app/modules/personal-account/pages/personal-account/personal-account.component.ts
+++ client/src/app/modules/personal-account/pages/personal-account/personal-account.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-	selector: 'app-welcome-personal-account',
+	selector: 'app-personal-account',
 	templateUrl: './personal-account.component.html',
 	styleUrls: ['./personal-account.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,

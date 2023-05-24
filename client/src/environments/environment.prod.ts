8cef80a5cc95075da5bc70239d22ce9c1f141ab3
Eduard Krivanek <krivanek1234@gmail.com>
2023-05-24 09:28:57 +0300

    chore: PA - mobile - display selected tag in button

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	version: '1.0.0',
+	version: '1.0.7',
 
 	backend_url: 'https://spend-mindful-graphql-6bhi5d4fdq-ew.a.run.app',
 	custom_graphql_backend_url: 'https://spend-mindful-graphql-6bhi5d4fdq-ew.a.run.app/graphql',

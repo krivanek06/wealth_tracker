41f337a8bea691d9a184a39ad7a96fcc01448cfe
Eduard Krivanek <krivanek1234@gmail.com>
2023-05-29 15:47:59 +0300

    feat: moved service-worker inicialization into the dashboard

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { ApplicationRef, Component, OnInit } from '@angular/core';
-import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
-import { concat, filter, first, interval } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { DialogServiceUtil } from './shared/dialogs';
 
 @Component({
@@ -9,40 +7,7 @@ import { DialogServiceUtil } from './shared/dialogs';
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-	constructor(dialogServiceUtil: DialogServiceUtil, private swUpdate: SwUpdate, private appRef: ApplicationRef) {}
+	constructor(dialogServiceUtil: DialogServiceUtil) {}
 
-	ngOnInit() {
-		if (this.swUpdate.isEnabled) {
-			this.updateWatcher();
-			this.checkServiceWorkerUpdate();
-		}
-	}
-
-	private checkServiceWorkerUpdate() {
-		this.swUpdate.versionUpdates
-			.pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
-			.subscribe(async () => {
-				if (await DialogServiceUtil.showConfirmDialog('New version is available. Load New Version?')) {
-					console.log('reloading');
-					window.location.reload();
-				}
-			});
-	}
-
-	private updateWatcher() {
-		// Allow the app to stabilize first, before starting
-		// polling for updates with `interval()`.
-		const appIsStable$ = this.appRef.isStable.pipe(first((isStable) => isStable === true));
-		const everySixHours$ = interval(6 * 60 * 60 * 1000);
-		const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
-
-		everySixHoursOnceAppIsStable$.subscribe(async () => {
-			try {
-				const updateFound = await this.swUpdate.checkForUpdate();
-				console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
-			} catch (err) {
-				console.error('Failed to check for updates:', err);
-			}
-		});
-	}
+	ngOnInit() {}
 }
--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ApplicationRef, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { Observable, concat, filter, first, interval } from 'rxjs';
 import { AccountManagerApiService } from '../../core/api';
 import { AccountManagerRoutes } from '../../core/models';
+import { DialogServiceUtil } from '../../shared/dialogs';
 
 @Component({
 	selector: 'app-dashboard',
@@ -12,9 +14,46 @@ import { AccountManagerRoutes } from '../../core/models';
 export class DashboardComponent implements OnInit {
 	availableAccounts$!: Observable<AccountManagerRoutes[]>;
 
-	constructor(private managerAccountApiService: AccountManagerApiService) {}
+	constructor(
+		private managerAccountApiService: AccountManagerApiService,
+		private swUpdate: SwUpdate,
+		private appRef: ApplicationRef
+	) {}
 
 	ngOnInit(): void {
 		this.availableAccounts$ = this.managerAccountApiService.getAvailableAccountRoutes();
+
+		if (this.swUpdate.isEnabled) {
+			this.updateWatcher();
+			this.checkServiceWorkerUpdate();
+		}
+	}
+
+	private checkServiceWorkerUpdate() {
+		this.swUpdate.versionUpdates
+			.pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+			.subscribe(async () => {
+				if (await DialogServiceUtil.showConfirmDialog('New version is available. Load New Version?')) {
+					console.log('reloading');
+					window.location.reload();
+				}
+			});
+	}
+
+	private updateWatcher() {
+		// Allow the app to stabilize first, before starting
+		// polling for updates with `interval()`.
+		const appIsStable$ = this.appRef.isStable.pipe(first((isStable) => isStable === true));
+		const everySixHours$ = interval(6 * 60 * 60 * 1000);
+		const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+		everySixHoursOnceAppIsStable$.subscribe(async () => {
+			try {
+				const updateFound = await this.swUpdate.checkForUpdate();
+				console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
+			} catch (err) {
+				console.error('Failed to check for updates:', err);
+			}
+		});
 	}
 }

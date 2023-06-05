3e43c0fd3c2bfdbe389e922ae72b7e68b73737af
Eduard Krivanek <krivanek1234@gmail.com>
2023-06-05 14:06:17 +0300

    chore: increase app version

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	version: '1.1.1',
+	version: '1.1.2',
 
 	backend_url: 'https://spend-mindful-graphql-6bhi5d4fdq-ew.a.run.app',
 	custom_graphql_backend_url: 'https://spend-mindful-graphql-6bhi5d4fdq-ew.a.run.app/graphql',

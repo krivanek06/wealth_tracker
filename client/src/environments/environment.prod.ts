04ca764f359ef5b68fa1436596d13bfcd1df9168
Eduard Krivanek <krivanek1234@gmail.com>
2024-01-21 15:55:21 +0100

    feat: upgrade packages

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -6,6 +6,15 @@ export const environment = {
 	custom_graphql_backend_url: 'https://spend-mindful-graphql-6bhi5d4fdq-ew.a.run.app/graphql',
 	custom_graphql_backend_ws: 'wss://spend-mindful-graphql-6bhi5d4fdq-ew.a.run.app/graphql',
 
+	firebaseConfig: {
+		apiKey: 'AIzaSyCF5EIL2kMhlvJAR7a6qJTXUi3tTmOoA8M',
+		authDomain: 'wealth-tracker-d2757.firebaseapp.com',
+		projectId: 'wealth-tracker-d2757',
+		storageBucket: 'wealth-tracker-d2757.appspot.com',
+		messagingSenderId: '721049469654',
+		appId: '1:721049469654:web:cc1d0e3ccff91180607f77',
+		measurementId: 'G-CM0D3TJEXS',
+	},
 	// backend_url: 'http://localhost:3000',
 	// custom_graphql_backend_url: 'http://localhost:3000/graphql',
 	// custom_graphql_backend_ws: 'ws://localhost:3000/graphql',
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -9,6 +9,16 @@ export const environment = {
 	backend_url: 'http://localhost:3000',
 	custom_graphql_backend_url: 'http://localhost:3000/graphql',
 	custom_graphql_backend_ws: 'ws://localhost:3000/graphql',
+
+	firebaseConfig: {
+		apiKey: 'AIzaSyCF5EIL2kMhlvJAR7a6qJTXUi3tTmOoA8M',
+		authDomain: 'wealth-tracker-d2757.firebaseapp.com',
+		projectId: 'wealth-tracker-d2757',
+		storageBucket: 'wealth-tracker-d2757.appspot.com',
+		messagingSenderId: '721049469654',
+		appId: '1:721049469654:web:cc1d0e3ccff91180607f77',
+		measurementId: 'G-CM0D3TJEXS',
+	},
 };
 
 /*
